4cf7c8d311a14489e1e0a2a21be268bd377afc75
Ivan Shimko <vana.shimko@gmail.com>
2019-05-17 09:54:53 +0300

    Initial commit

---
+++ MutableString.Tests/MutableString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MutableString.Tests
+{
+    [TestFixture]
+    public class MutableStringTest
+    {
+        [Test]
+        public void ShouldChangeString()
+        {
+            var mutableString = new MutableString("test");
+            
+            mutableString[0] = 'o';
+
+            Assert.That("test", Is.EqualTo("teot"));
+        }
+    }
+}
